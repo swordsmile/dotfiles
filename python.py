d3554e70fa53a7bd315252714d440268fa2a16c1
swordsmile <swordsmile@swordsmiledeiMac.local>
2017-04-11 23:32:07 +0800

    python template by create a new file

---
+++ python.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################
+# File Name:
+# Author: swordsmile
+# Mail: 1627989802@qq.com
+# Created Time:
+###################################
